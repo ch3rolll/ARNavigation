47d3bca5b18448a48ad4a8364c4cb5c23e2bc208
ch3roll <qi@ch3rolls-MacBook-Pro.local>
2018-09-11 18:41:07 -0700

    first commit

--- ARTry/ViewController.swift
+++ ARTry/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    var steps: [MKRouteStep] = []
+    var destinationLocation: CLLocationCoordinate2D!
+    var locationService = LocationService()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.session.run(configuration)
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.autoenablesDefaultLighting = true
+        
+        
+        //
+        var navService = NavigationService()
+        
+        self.destinationLocation = CLLocationCoordinate2D(latitude: 37.4073023, longitude: -122.0809194,17)
+        var request = MKDirectionsRequest()
+        
+        if destinationLocation != nil {
+            navService.getDirections(destinationLocation: destinationLocation, request: request) { steps in
+                for step in steps {
+                    self.steps.append(step)
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.geometry?.firstMaterial?.diffuse.contents = UIImage(named:"image")
+        node.geometry?.firstMaterial?.locksAmbientWithDiffuse = true
+        
+        node.position = SCNVector3(0,0,-0.3)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    @IBAction func addCup(_ sender: Any) {
+        let cupNode = SCNNode()
+        
+        let cc = getCameraCoordinates(sceneView: sceneView)
+        cupNode.position = SCNVector3(cc.x, cc.y, cc.z)
+        
+        guard let virtualObjectScene = SCNScene(named: "cup.scn", inDirectory: "Models.scnassets/cup") else {
+            return
+        }
+        
+        let wrapperNode = SCNNode()
+        for child in virtualObjectScene.rootNode.childNodes {
+            child.geometry?.firstMaterial?.lightingModel = .physicallyBased
+            wrapperNode.addChildNode(child)
+        }
+        cupNode.addChildNode(wrapperNode)
+        
+        self.sceneView.scene.rootNode.addChildNode(cupNode)
+    }
+    
+    struct myCameraCoordinates {
+        var x = Float()
+        var y = Float()
+        var z = Float()
+    }
+    
+    func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
+        let cameraTransform = sceneView.session.currentFrame?.camera.transform
+        let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
+        
+        var cc = myCameraCoordinates()
+        cc.x = cameraCoordinates.translation.x
+        cc.y = cameraCoordinates.translation.y
+        cc.z = cameraCoordinates.translation.z
+        
+        return cc
+    }
+    
 }
 
